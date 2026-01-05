171a0068743ecb31f91aab5ffe69bc22b2611467
oscar818super <oscar818super@gmail.com>
2026-01-05 19:32:59 +0800

    Add files via upload

--- RB_software_project/src/VCUStates.cpp
+++ RB_software_project/src/VCUStates.cpp
@@ -8,11 +8,30 @@ VCUState currentState = VCUState::INIT;
 const int BRAKE_THRESHOLD = 200;    // Subject to Change
 const unsigned long STARTIN_DURATION_MS = 2000;
 const unsigned long BUZZIN_DURATION_MS = 2000;
+const unsigned int APPS_FAULTY_DURATION_MS = 100;
+const bool isFrontDirection = true;
 
 // Define Timers
 static unsigned long timerState = 0;
+static unsigned long timerAPPSFault = 0;
+static bool isAppsFaultActive = false;
 
-// Public Functions
+//  Private Functions
+int16_t calculateTorque(int throttleVal) {
+    
+    throttleVal = constrain(throttleVal, 0, 1023);
+    long mappedVal = 0;
+    if (isFrontDirection) {
+        mappedVal = map(throttleVal, 0, 1023, 0, 32767);
+    } else {
+
+        mappedVal = map(throttleVal, 0, 1023, 0, -32768); 
+    }
+    
+    return (int16_t)mappedVal;
+}
+
+//  Public Functions
 void initStateMachine() {
     currentState = VCUState::INIT;
     digitalWrite(PIN_DRIVE_MODE_LED, LOW);
@@ -25,6 +44,8 @@ void updateStateMachine() {
     // Read inputs
     int startButtonState = digitalRead(PIN_START_BUTTON);
     int brakePedalValue = analogRead(PIN_BRAKES);
+    int apps5VValue = analogRead(PIN_APPS_5V);
+    int apps3V3Value = analogRead(PIN_APPS_3V3);
 
     if (brakePedalValue > BRAKE_THRESHOLD) {
         digitalWrite(PIN_BRAKE_LIGHT, HIGH);
@@ -76,6 +97,31 @@ void updateStateMachine() {
 
         case VCUState::DRIVE:
 
+            int apps3v3ValueScaled = apps3V3Value * 5 / 3.3;
+
+            if (abs(apps5VValue - apps3v3ValueScaled) / apps5VValue > 0.1) {
+                if (isAppsFaultActive && (millis() - timerAPPSFault >= APPS_FAULTY_DURATION_MS)) {
+                    currentState = VCUState::INIT;
+                    isAppsFaultActive = false;
+                    break;
+                } 
+                
+                else if (!isAppsFaultActive) {
+                    isAppsFaultActive = true;
+                    timerAPPSFault = millis();
+                }
+            }
+
+            else {
+                isAppsFaultActive = false;
+            }
+
+            int16_t torqueCommand = calculateTorque(apps5VValue);
+
+            break;
+            
+
 
     }
+
 }
